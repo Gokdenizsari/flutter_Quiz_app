30f969df638e2723d899df12f1a827fc936def76
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-09-19 14:56:13 +0300

    Create progress.dart

---
+++ lib/components/progress.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:quiz_app/controllers/question_controller.dart';
+import 'package:quiz_app/main/constants_part/constants.dart';
+import 'package:websafe_svg/websafe_svg.dart';
+
+class Progress extends StatelessWidget {
+  const Progress({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 40,
+      decoration: BoxDecoration(
+        border: Border.all(width: 4),
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: GetBuilder<QuestionController>(
+          init: QuestionController(),
+          builder: (controller) {
+            return Stack(children: [
+              LayoutBuilder(
+                builder: (context, constraints) => Container(
+                  width: constraints.maxWidth * controller.animation.value,
+                  decoration: BoxDecoration(
+                    gradient: gPrimaryGradient,
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+              ),
+              Positioned.fill(
+                  child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: gDefaultPadding / 2),
+                child: Row(
+                  children: [
+                    Text("${(controller.animation.value).round()} sec"),
+                    WebsafeSvg.asset("assets/icons/clock.svg"),
+                  ],
+                ),
+              ))
+            ]);
+          }),
+    );
+  }
+}
