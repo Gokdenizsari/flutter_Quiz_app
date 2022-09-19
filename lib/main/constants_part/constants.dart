bc2fa0ce320a9db2873525260eb3af38b2512773
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-09-19 13:24:10 +0300

    Create constants.dart

---
+++ lib/main/constants_part/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const gSecondaryColor = Color.fromARGB(255, 157, 169, 224);
+const gGreenColor = Color.fromARGB(255, 127, 226, 107);
+const gRedColor = Color.fromARGB(255, 230, 63, 66);
+const gGrayColor = Color.fromARGB(255, 218, 214, 214);
+const gBlackColor = Color.fromARGB(255, 12, 12, 12);
+const gPrimaryGradient = LinearGradient(
+  colors: [Color.fromARGB(255, 86, 200, 218), Color.fromARGB(255, 1, 204, 163)],
+  begin: Alignment.centerLeft,
+  end: Alignment.centerRight,
+);
+
+const double gDefaultPadding = 20.0;
